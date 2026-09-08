e1e25362764723da65332373377cabd9fa292c3c
ouyang <jxfengzi@gmail.com>
2026-09-08 12:10:10 +0800

    修改版本，增加 TODO.md

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  server: 'https://matrix.openxiot.cn',
+  server: 'http://localhost:8080',
   account: 'https://account.openxiot.cn',
   product: 'https://product.openxiot.cn',
 };
